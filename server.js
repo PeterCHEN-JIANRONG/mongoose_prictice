14fc3db2e7dc128e45a2e351c94519081335b9d3
peter.chen1024 <u0052017@gmail.com>
2022-04-11 18:00:31 +0800

    add schema, model

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ mongoose.connect('mongodb://localhost:27017/hotel').then(()=>{
   console.log(err.reason); // 連線失敗
 })
 
+const roomSchema = {
+  name: String,
+  price: {
+    type: Number,
+    required: [true, "價格必填"]
+  },
+  rating: Number
+}
+
+const Room = mongoose.model('Room', roomSchema);
+// mongoose 會自動修正 collection 的名稱
+// 開頭小寫、自動加 s
+// Room > rooms
+// 若不要這樣，可以透過 Schema 修改規則
+
 const requestListener = (req, res) => {
   console.log(req.url);
   res.end();
